f34394a60fce560c11babbdd3ffbad41c25f13bb
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2025-12-22 01:13:42 -0500

    simplify the list ui
    code is gross

--- pkg/tui/tui.go
+++ pkg/tui/tui.go
@@ -45,10 +45,27 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	m.list, cmd = m.list.Update(msg)
 
 	// Keep a snapshot of the last selected item to preserve while filtering
-	if m.list.FilterState().String() != "filtering" {
+	if !m.list.SettingFilter() {
 		if item, ok := m.list.SelectedItem().(item); ok {
 			m.selectedItem = item
 		}
+	}
+
+	if m.list.SettingFilter() {
+		m.list.Styles.TitleBar = m.list.Styles.TitleBar.
+			BorderForeground(lipgloss.AdaptiveColor{Light: "#F793FF", Dark: "#AD58B4"})
+	} else {
+		m.list.Styles.TitleBar = m.list.Styles.TitleBar.
+			BorderForeground(lipgloss.AdaptiveColor{Light: "#C2B8C2", Dark: "#4D4D4D"})
+	}
+
+	style := lipgloss.NewStyle().
+		UnsetBackground().
+		Foreground(lipgloss.AdaptiveColor{Light: "#A49FA5", Dark: "#777777"})
+	if m.list.FilterValue() == "" {
+		m.list.Title = Kbd("/") + style.Render("Search Stations")
+	} else {
+		m.list.Title = Kbd("/") + style.Render(m.list.FilterValue())
 	}
 
 	return m, cmd
@@ -69,6 +86,14 @@ func RouteBadge(route gtfs.Route) string {
 	return style.Render(route.RouteShortName)
 }
 
+func Kbd(key string) string {
+	style := lipgloss.NewStyle().
+		MarginRight(1).
+		Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#dddddd"})
+
+	return style.Render(key)
+}
+
 func main() {
 	schedule := gtfs.GetSchedule()
 	stations := schedule.GetStations()
@@ -86,8 +111,26 @@ func main() {
 	}
 
 	list := list.New(items, list.NewDefaultDelegate(), 0, 0)
-	list.Title = "Stations"
+
 	list.SetShowPagination(false)
+	list.SetShowHelp(false)
+	list.SetShowStatusBar(false)
+
+	list.Styles.TitleBar = lipgloss.NewStyle().
+		Width(40).
+		Padding(0, 1).
+		MarginBottom(1).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.AdaptiveColor{Light: "#C2B8C2", Dark: "#4D4D4D"})
+
+	list.Styles.Title = lipgloss.NewStyle().
+		UnsetBackground().
+		Foreground(lipgloss.AdaptiveColor{Light: "#A49FA5", Dark: "#777777"})
+
+	list.FilterInput.Prompt = Kbd("/")
+	list.FilterInput.Placeholder = "Search Stations"
+	list.FilterInput.TextStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#dddddd"})
 
 	m := model{
 		list:         list,
